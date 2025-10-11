d121d62f1076baba4522e20df7e097727ee3d915
ikuto123 <c1307768@st.kanazawa-it.ac.jp>
2025-10-11 09:44:03 +0900

    AI_SearchEnemyStateの句tくクラス名ミスの修正

--- Assets/Script/Enemy/state/AI_Search_EnemyState.cs
+++ Assets/Script/Enemy/state/AI_Search_EnemyState.cs
@@ -1,11 +1,11 @@
 using AIState;
 using UnityEngine;
 
-public class AI_Search_EnemyState
+public class AI_SearchEnemyState
 {
     private AIController _ai;
     
-    public AI_Search_EnemyState(AIController ai)
+    public AI_SearchEnemyState(AIController ai)
     {
         _ai = ai;
     }
