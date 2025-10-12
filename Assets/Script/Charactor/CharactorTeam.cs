c9d04352c96bb67e6c5b1df3359a21294e5e394c
ikuto123 <c1307768@st.kanazawa-it.ac.jp>
2025-10-12 09:59:47 +0900

    #29 発射位置と味方に当たらないようにした

--- Assets/Script/Charactor/CharactorTeam.cs
+++ Assets/Script/Charactor/CharactorTeam.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CharactorTeam : MonoBehaviour
+public class CharactorTeam : MonoBehaviour , ITeamAffiliated
 {
     [Tooltip("所属するチームの選択")]
     [SerializeField] private Team team;
---
+++ Assets/Script/intface/ITeamAffiliated.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ITeamAffiliated
+{
+    Team Team { get; }
+}
