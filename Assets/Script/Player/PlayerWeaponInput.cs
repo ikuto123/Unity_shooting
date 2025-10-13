a72b2849228571f1d953d4325ecc08b368591819
ikuto123 <c1307768@st.kanazawa-it.ac.jp>
2025-10-13 16:25:30 +0900

    銃が2å同時に撃たれ鵜のを修正

--- Assets/Script/Player/PlayerWeaponInput.cs
+++ Assets/Script/Player/PlayerWeaponInput.cs
@@ -4,12 +4,11 @@ public class PlayerWeaponInput
 {
     public void GunHolder(CharacterManager character)
     {
+        //Debug.Log("GunHolder is called. Frame: " + Time.frameCount);
         if (Input.GetKeyDown(KeyCode.Alpha1)) { character.WeaponManager.SelectGun(1); }
         else if (Input.GetKeyDown(KeyCode.Alpha2)) { character.WeaponManager.SelectGun(2); }
         else if (Input.GetKeyDown(KeyCode.Alpha3)) { character.WeaponManager.SelectGun(3); }
         
-        if (Input.GetMouseButtonDown(0)){ character.WeaponManager.FireCurrentGun(character); }
-        if (Input.GetMouseButton(0)) {character.WeaponManager?.FireCurrentGun(character);}
-        
+        if (Input.GetMouseButton(0)) { character.WeaponManager?.FireCurrentGun(character); }
     }
 }
