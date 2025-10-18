b4b1e4a5fed987bc0496c7e0e87ce0ecbdc46e8d
ikuto123 <c1307768@st.kanazawa-it.ac.jp>
2025-10-18 09:49:09 +0900

    敵が銃を切れ変えられるようにしたã

--- Assets/Script/Player/PlayerInputController.cs
+++ Assets/Script/Player/PlayerInputController.cs
@@ -16,7 +16,7 @@ public class PlayerInputController : MonoBehaviour
     [SerializeField] private CinemachineCamera _FPSCamera;
     
     private Transform _player;
-    private float _sensitivity = 3f;//カメラ感度
+    private float _sensitivity = 3.5f;//カメラ感度
     
     CharacterManager _characterManager;
     private void Start()
@@ -40,6 +40,7 @@ public class PlayerInputController : MonoBehaviour
             return;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift)) { _playerAnimator.Squat(false); }
+        
         _playerWeaponInput.GunHolder(_characterManager);
         _playerMoveInput.ReadInput();
         _playerAnimator.UpdateMovementAnimation(_playerMoveInput.MoveDirection);
